ac19870330744e12c7974bb89a77b01fcf70a539
Hirokixd <86230878+Wrdn28@users.noreply.github.com>
2024-02-24 21:38:17 +0700

    Ada some build flags

--- hz14x01ad.sh
+++ hz14x01ad.sh
@@ -7,7 +7,14 @@ repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --pr
 # Set up build environment
 export BUILD_HOSTNAME=crave
 export BUILD_USERNAME=Hirokixd
+export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
+export BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE=true
+export BUILD_BROKEN_CLANG_PROPERTY=true
+export BUILD_BROKEN_CLANG_ASFLAGS=true
+export BUILD_BROKEN_CLANG_CFLAGS=true
+export BUILD_BROKEN_PYTHON_IS_PYTHON2=true
+export BUILD_BROKEN_USES_SOONG_PYTHON2_MODULES=true
 export TZ=Asia/Jakarta
 source build/envsetup.sh
 # Lunch configuration
